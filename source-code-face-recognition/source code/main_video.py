398dd2d909d8504a862c8a51983964732f2d2bb9
agnesrohtsalu <agnesroht@gmail.com>
2023-12-03 21:28:10 +0200

    töötab, vaja kirjutada sõnastik inimeste kirjeldustega funktsiooni

--- source-code-face-recognition/source code/main_video.py
+++ source-code-face-recognition/source code/main_video.py
@@ -3,10 +3,10 @@ from simple_facerec import SimpleFacerec
 
 # Encode faces from a folder
 sfr = SimpleFacerec()
-sfr.load_encoding_images("images/")
+sfr.load_encoding_images("source-code-face-recognition/source code/images")
 
 # Load Camera
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 
 while True:
@@ -14,11 +14,11 @@ while True:
 
     # Detect Faces
     face_locations, face_names = sfr.detect_known_faces(frame)
-    for face_loc, name in zip(face_locations, face_names):
+    for face_loc, name in zip(face_locations, face_names): #nimed mida kuvatakse võetakse pildi pealkirjast
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
-        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2) #teksti värv on must ja teksti paksus on 2 pikslit
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4) 
 
     cv2.imshow("Frame", frame)
 
